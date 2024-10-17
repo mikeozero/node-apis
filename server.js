1fa7fb70bd656814dd27575b50a49bbe6a88530b
mike <mikezerone@hotmail.com>
2024-10-17 00:12:33 -0400

    update all request method

--- server.js
+++ server.js
@@ -3,10 +3,37 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.get('/getUser',(req,res)=>{
-  console.log('GET USER...');
-  res.send('some data from getUser...')
+app.get('/getUserQuery',(req,res)=>{
+  console.log('get query data: ',req.query);
+  res.send('some data from getUserQuery...')
+})
+
+app.post('/postUserQuery',(req,res)=>{
+  console.log('post query data: ',req.query);
+  res.send('some data from postUserQuery...')
+})
+
+app.get('/getUserParams/:name/:age',(req,res)=>{
+  console.log('get params data: ',req.params);
+  res.send('some data from getUserParams...')
+})
+
+app.post('/postUserParams/:name/:age',(req,res)=>{
+  console.log('post params data: ',req.params);
+  res.send('some data from postUserParams...')
+})
+
+app.post('/postUserBody',(req,res)=>{
+  console.log('post body data: ',req.body);
+  res.send('some data from postUserBody...')
+})
+
+app.post('/postUserQueryParamsBody/:name',(req,res)=>{
+  console.log('all request data: ',req.query,'-',req.params,'-',req.body);
+  res.send('some data from postQueryParamsBody...')
 })
 
 // app.get("/:universalURL", (req, res) => {
